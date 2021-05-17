96b08ef47d91992db0eab49b5decd2461b87d2dd
Chennuri Ullas Reddy <ullasreddy3663@gmail.com>
2021-05-17 13:46:45 +0530

    Update Find sum of divisors.cpp

--- Find sum of divisors.cpp
+++ Find sum of divisors.cpp
@@ -3,7 +3,6 @@
 Given a natural number N, the task is to find the sum of the divisors of all the divisors of N.
 
 Example 1:
-
 Input:
 N = 54
 Output:
@@ -14,8 +13,9 @@ Sum of divisors of 1, 2, 3, 6, 9, 18, 27, 54
 are 1, 3, 4, 12, 13, 39, 40, 120 respectively.
 Sum of divisors of all the divisors of 54
  = 1 + 3 + 4 + 12 + 13 + 39 + 40 + 120 = 232.
+ 
+ 
 Example 2:
-
 Input:
 N = 10
 Output:
@@ -25,8 +25,10 @@ Divisors of 10 are 1, 2, 5, 10
 Sums of divisors of all the divisors are 
 1, 3, 6, 18.
 Overall sum = 1 + 3 + 6 + 18 = 28
+
 Your Task:  
-You don't need to read input or print anything. Your task is to complete the function sumOfDivisors() which takes an integer N as an input parameter and return the sum of the divisors of all the divisors of N.
+You don't need to read input or print anything. Your task is to complete the function sumOfDivisors() which takes an integer N 
+as an input parameter and return the sum of the divisors of all the divisors of N.
 
 Expected Time Complexity: O(NloglogN)
 Expected Auxiliary Space: O(N)
