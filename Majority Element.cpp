cb57c83a71526e8973fffc02fe56a45fd6eae485
Chennuri Ullas Reddy <ullasreddy3663@gmail.com>
2021-05-28 19:20:26 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,71 @@
+/*
+Given an array A of N elements. Find the majority element in the array. 
+A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+
+
+Example 2:
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+Your Task:
+The task is to complete the function majorityElement() which returns the majority element in the array.
+If no majority exists, return -1.
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+ 
+
+Constraints:
+1 <= N <= 10^7
+0 <= Ai <= 10^6
+*/
+
+int majorityElement(int arr[], int n)
+{
+        
+    // your code here
+    int maj_ele=arr[0];
+    int count=1;
+    
+    // Moore's vooting algorithm to find majority element in array 
+    for(int i=1;i<n;i++){
+        if(maj_ele == arr[i]){
+            count++;
+        }else{
+            count--;
+        }
+        if(count==0){
+            maj_ele = arr[i];
+            count=1;
+        }
+    }
+    
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(maj_ele == arr[i]){
+            c++;
+        }
+    }
+    return c>n/2? maj_ele : -1; // if maj_ele exists more than half times....
+}
