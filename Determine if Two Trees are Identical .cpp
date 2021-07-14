3243dfe87b08043e4bc73fc7ff89da329391be83
Chennuri Ullas Reddy <ullasreddy3663@gmail.com>
2021-07-14 16:20:18 +0530

    Create Determine if Two Trees are Identical .cpp

---
+++ Determine if Two Trees are Identical .cpp
@@ -0,0 +1,60 @@
+/*
+
+Given two binary trees, the task is to find if both of them are identical or not. 
+
+
+Example 1:
+Input:
+     1          1
+   /   \      /   \
+  2     3    2     3
+Output: Yes
+Explanation: There are two trees both
+having 3 nodes and 2 edges, both trees
+are identical having the root as 1,
+left child of 1 is 2 and right child
+of 1 is 3.
+
+
+Example 2:
+Input:
+    1       1
+  /  \     /  \
+ 2    3   3    2
+Output: No
+Explanation: There are two trees both
+having 3 nodes and 2 edges, but both
+trees are not identical.
+
+Your task:
+Since this is a functional problem you don't have to worry about input,
+you just have to complete the function isIdentical() that takes two roots as parameters and returns true or false. 
+The printing is done by the driver code.
+
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <=Data of a node <= 105
+
+
+*/
+
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if(r1==NULL && r2==NULL){
+            return true;
+        }
+        if(r1!=NULL && r2==NULL || r2!=NULL&&r1==NULL){
+            return false;
+        }
+        if(r1->data != r2->data){
+            return false;
+        }else{
+            return isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right);
+        }
+    }
